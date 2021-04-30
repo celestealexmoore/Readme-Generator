4de64955b35755912bbe27d0c93a56b2c4c8ebab
Celeste Moore <celestealexmoore@gmail.com>
2021-04-30 09:25:34 -0500

    Starting in index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    'What is the Title of your project?',
+    'Describe your project:',
+    'Installation Instructions',
+    'List the names of all contributors separated by a comma:',
+    'What is your email?',
+    'What is your Github username?'
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

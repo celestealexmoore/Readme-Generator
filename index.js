2bff317d23a01d077dea4ddf2b5fd1aadc6beb4a
Celeste Moore <celestealexmoore@gmail.com>
2021-04-30 00:33:34 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+console.log("Hello From The Other Side")
+
+console.log("User initiates application");
+console.log("User is asked about README sections");
+console.log("User responses are logged");
+console.log("README.md file with user responses produced");

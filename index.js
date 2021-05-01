d3b085d6b7f4d6b92bdd0e5d0dcae217f9215fcd
Celeste Moore <celestealexmoore@gmail.com>
2021-05-01 16:59:59 -0500

    Not sure why recent changes didn't push

--- index.js
+++ index.js
@@ -1,14 +1,71 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [
-    'What is the Title of your project?',
-    'Describe your project:',
-    'Installation Instructions',
-    'List the names of all contributors separated by a comma:',
-    'What is your email?',
-    'What is your Github username?'
-];
+
+inquirer.prompt([
+    {
+        name: 'Title',
+        message: 'What is the Title of your project?',
+        type: 'input',
+    },
+
+    {
+        name: 'Description',
+        message: 'Describe your project:',
+        type: 'input',
+    },
+
+    {
+        name: 'Media',
+        message: 'Please paste file paths to images/videos in the order that they should appear:',
+        type: 'input',
+    },
+
+    {
+        name: 'Installation',
+        message: 'Installation Instructions:',
+        type: 'input',
+    },
+
+    {        name: 'Contributors',
+        message: 'List the names of all contributors separated by a comma:',
+        type: 'input',
+    },
+
+    {
+        name: 'Contact',
+        message: 'What is your Github username?',
+        type: 'input',
+    },
+
+    {
+        name: 'Future Development',
+        message: 'What are your ideas for future development of this project?',
+        type: 'input',
+    },
+
+    {
+        name: 'Licensure',
+        message: 'How is this project licensed?',
+        type: 'input',
+    },
+
+    {
+        name: 'Project Status',
+        message: 'What is your project status? (Completed, Slowed Down, Stopped)',
+        type: 'input',
+    }])
+
+.then(function(answer){
+ console.log(answer);
+});
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
